1f8b6588b4782f624de241965fa06bba661857ab
Alanbaba <38888487+Alanbaba2018@users.noreply.github.com>
2019-08-19 16:18:25 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/menu', require('./menu/menu.js'));
-router.use('/oauth', require('./oauth/auth.js'));
+//router.use('/oauth', require('./oauth/auth.js'));
 router.use('/upload', require('./upload/upload.js'));
 module.exports = router;
